ced78cc9c37f76cc8e28a76d875a5be77fcc496f
Raynerenyar <105347568+Raynerenyar@users.noreply.github.com>
2023-11-13 01:44:32 +0800

    Added new field into Chat.java, added new /calculate command and its implementation, refined CronJobService.java, and created SubscriberService.java

--- src/main/java/org/telegram/toto/TotoApplication.java
+++ src/main/java/org/telegram/toto/TotoApplication.java
@@ -12,34 +12,18 @@ import org.telegram.toto.repository.ChatRepo;
 import org.telegram.toto.repository.entities.Chat;
 
 @SpringBootApplication(scanBasePackages = {
-		"org.telegram.toto.service",
-		"org.telegram.toto.repository",
-		"org.telegram.toto.controller",
-		"org.telegram.toto.config",
-		"org.telegram.toto.cron" })
+        "org.telegram.toto.service",
+        "org.telegram.toto.repository",
+        "org.telegram.toto.controller",
+        "org.telegram.toto.config",
+        "org.telegram.toto.cron"})
 @EntityScan("org.telegram.toto.repository.entities")
 @EnableScheduling
-public class TotoApplication implements CommandLineRunner {
+public class TotoApplication {
 
-	@Autowired
-	private ChatRepo chatRepo;
+    public static void main(String[] args) {
+        SpringApplication.run(TotoApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(TotoApplication.class, args);
-
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		List<String> chats1 = chatRepo.findAllByAlertValueNextDrawReceivedFalse(1000000);
-		List<String> chats2 = chatRepo.findAllByAlertValueNextDrawReceivedTrue(1000000);
-
-		for (String chat : chats1) {
-			System.out.println(chat);
-		}
-		for (String chat : chats2) {
-			System.out.println(chat);
-		}
-	}
+    }
 
 }
